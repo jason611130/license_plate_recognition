30d8d6e841bdc298140048198bf1f79391409b8c
Pppurin <aly4353@gmail.com>
2023-09-22 21:55:04 +0800

    binarization

--- test/opencv.c
+++ test/opencv.c
@@ -1,41 +1,33 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgcodecs/imgcodecs.hpp"
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <cstdlib>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/highgui/highgui_c.h>
 using namespace cv;
-
-int main()
-{
-    Mat inputImage = imread("C://Users//凱駿//Documents//GitHub//license_plate_recognition//test//testcar1.bmp",IMREAD_COLOR);
-    cvtColor(inputImage, gray, COLOR_BGR2GRAY);
-    //find threshold
-    for(size_t y=0;y<gray.rows;y+=block){
-        for(size_t x=0;x<gray.cols;x+=block){
-            tot=0;
-            for(int i=0;i<block;i++){
-                for(int j=0;j<block;j++){
-                    unsigned char* row_ptr=gray.data+(y+i)*gray.cols;
-                    tot+=row_ptr[x+j];
-                }
-            }
-            th=tot/(block*block)*0.6;
-    //binarization
-            for(int i=0;i<block;i++){
-                for(int j=0;j<block;j++){
-                    unsigned char* row_ptr=gray.data+(y+i)*gray.cols;
-                    if(row_ptr[x+j]>th){
-                        row_ptr[x+j]=255;
-                    }
-                    else{
-                        row_ptr[x+j]=0;
-                    }
-                }
-            }
-        }
-     }
-    imshow("Gray",gray);
+using namespace std;
+int main(int argc, const char * argv[]) {
+    Point2f srcTri[4];
+    Point2f dstTri[4];
+    Mat warpPerspective_mat(3,3,CV_32FC1);
+    Mat src = imread("/Users/michelle/Documents/Michelle/NTUST專題/car.png",0);
+    Mat warpPerspective_dst;
+    warpPerspective_dst=Mat::zeros(src.rows,src.cols,src.type());
     
-    cout<<"hello world\n";
-    cout<<"test";
+    srcTri[0]=Point2f(src.cols*0.45,src.rows*0.65);
+    srcTri[1]=Point2f(src.cols*0.65,src.rows*0.8);
+    srcTri[2]=Point2f(src.cols*0.45,src.rows*0.8);
+    srcTri[3]=Point2f(src.cols*0.65,src.rows*0.95);
+    
+    dstTri[0]=Point2f(src.cols*0.2,src.rows*0.3);
+    dstTri[1]=Point2f(src.cols*0.8,src.rows*0.4);
+    dstTri[2]=Point2f(src.cols*0.17,src.rows*0.79);
+    dstTri[3]=Point2f(src.cols*0.815,src.rows*0.82);
+    warpPerspective_mat=getPerspectiveTransform(srcTri,dstTri);
+    warpPerspective(src,warpPerspective_dst,warpPerspective_mat,src.size());
+    imshow("inputImg",src);
+    imshow("outputImg",warpPerspective_dst);
+   
+    waitKey(0);
+    return 0;
 }
