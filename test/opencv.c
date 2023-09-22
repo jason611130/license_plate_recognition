89ecab65536c33449f508465cf240ea1c20aa827
Manson <106231624+Manson1324@users.noreply.github.com>
2023-09-22 23:12:03 +0800

    BGR2_GRAY
    軒姐我的神

--- test/opencv.c
+++ test/opencv.c
@@ -4,7 +4,22 @@
 #include<iostream>
 using namespace std;
 using namespace cv;
+Mat transform(Mat A, Mat H)
+{
+// allocate array of all locations
+int Numrows = A.rows;
+int Numcols = A.cols;
+int channels   = A.channels();
+cout << "rows " << Numrows << "col " << Numcols << "channels " << channels <<endl;
+int size = Numrows*Numcols;
+int MaxX,MaxY = -1000;
+int MinX,MinY =  1000;
+int *TransArry = (int *)malloc(sizeof(int)*size);
+int Idx;
 
+int homeX=Idx % Numcols;
+int homeY=Idx / Numcols;
+cout << H << endl;
 Mat Picture2Gray(Mat im2gray) {
     Mat im1(im2gray.rows, im2gray.cols,CV_8U);
     for (int i = 0; i < im2gray.rows; i++) {
@@ -14,12 +29,53 @@ Mat Picture2Gray(Mat im2gray) {
     }
     return im1;
 }
+waitKey();
+for (Idx=0; Idx < size; ++Idx ){
+
+       homeX=Idx % Numcols;
+       homeY=Idx / Numcols;
+
+       float x  = (H.at<float>(0,0) * (homeX)) +( H.at<float>(0,1) * (homeY)) + ( H.at<float>(0,2) * 1) ;
+       float y  = (H.at<float>(1,0) * (homeX)) +( H.at<float>(1,1) * (homeY)) + ( H.at<float>(1,2) * 1) ;
+       float s  = (H.at<float>(2,0) * (homeX)) +( H.at<float>(2,1) * (homeY)) + ( H.at<float>(2,2) * 1) ;
+
+       cout << " x = " << x << " y= " << y << " s= " << s;
+       x = (x/s);
+
+       y = y/s;
+
+       // for the first col in TransMatrix
+       if (homeX ==0){
+           if (x > MaxX) MaxX = x;
+           if (x < MinX) MinX = x;
+       }
+
+       //for thee first row in TransMatrix
+       if (homeY ==0){
+           if (y > MaxY) MaxY = y;
+           if (y < MinY) MinY = y;
+       }
+       if((y)>=A.rows || (y)<0 || (x)>=A.cols || (x)<0){
+           TransArry[Idx]  = -1;
+           cout << "x= " << x << "y= "<< y << endl;
+       }else{
+           TransArry[Idx] = (y * Numcols + x);
+       }
+
+       //cout << Numcols << endl;
+       cout <<     "New index of " << Idx << "is " << TransArry[Idx] << endl;
+       }
+
+        Mat   tranImg ;
+
+        A.copyTo(tranImg);
+        tranImg = tranImg - tranImg;
+       cout <<     "Rows" << tranImg.rows << "cols" << tranImg.cols << "cha" <<  A.channels() << endl;
+
 int main()
 {
     Mat inputImage = imread("C://Users//凱駿//Documents//GitHub//license_plate_recognition//test//testcar1.bmp",IMREAD_COLOR);
 
-<<<<<<< HEAD
-=======
        waitKey();
        // Remap Image
        for (Idx=0; Idx < size; Idx ++ ){
@@ -62,7 +118,6 @@ int main()
     Mat inputImage = imread("C://Users//凱駿//Documents//GitHub//license_plate_recognition//test//testcar1.bmp",IMREAD_COLOR);
     Mat outputImg;
     transform(inputImage,outputImg);
->>>>>>> 1125011182f25ef031e4a5e27d2f30f095a3d3b4
     cout<<"hello world\n";
     cout<<"test";
 }
