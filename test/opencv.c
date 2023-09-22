ff10183d1686111460104991026597830ab97477
Manson <106231624+Manson1324@users.noreply.github.com>
2023-09-22 21:38:20 +0800

    read_picture

--- test/opencv.c
+++ test/opencv.c
@@ -1,7 +1,14 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs/imgcodecs.hpp"
 #include<iostream>
+using namespace std;
+using namespace cv;
 
+int main()
+{
+    Mat inputImage = imread("C://Users//凱駿//Documents//GitHub//license_plate_recognition//test//testcar1.bmp",IMREAD_COLOR);
 
-int main(){
     cout<<"hello world\n";
     cout<<"test";
 }
